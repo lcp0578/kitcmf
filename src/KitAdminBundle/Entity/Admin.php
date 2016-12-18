f3c22e2426799e23ec3f936d8ecff249618b870e
李春鹏 <lcp0578@gmail.com>
2016-12-18 20:00:28 +0800

    [MOD] implement UserInterface

--- src/KitAdminBundle/Entity/Admin.php
+++ src/KitAdminBundle/Entity/Admin.php
@@ -1,9 +1,8 @@
 <?php
-
 namespace KitAdminBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use FOS\UserBundle\Model\User as BaseUser;
+use FOS\UserBundle\Model\UserInterface;
 
 /**
  * User
@@ -11,94 +10,82 @@ use FOS\UserBundle\Model\User as BaseUser;
  * @ORM\Table(name="admin")
  * @ORM\Entity(repositoryClass="KitAdminBundle\Repository\AdminRepository")
  */
-class Admin extends BaseUser
+class Admin implements UserInterface
 {
+
     /**
-     * @var int
      *
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
+     * @var int @ORM\Column(name="id", type="integer")
+     *      @ORM\Id
+     *      @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
 
     /**
-     * @var string
      *
-     * @ORM\Column(name="username", type="string", length=60, unique=true)
+     * @var string @ORM\Column(name="username", type="string", length=60, unique=true)
      */
-    //protected $username;
+    protected $username;
 
     /**
-     * @var string
      *
-     * @ORM\Column(name="password", type="string", length=120)
+     * @var string @ORM\Column(name="password", type="string", length=120)
      */
-    //protected $password;
+    protected $password;
 
     /**
-     * @var string
      *
-     * @ORM\Column(name="truename", type="string", length=30, nullable=true)
+     * @var string @ORM\Column(name="truename", type="string", length=30, nullable=true)
      */
     private $truename;
 
     /**
-     * @var string
      *
-     * @ORM\Column(name="cardid", type="string", length=18)
+     * @var string @ORM\Column(name="cardid", type="string", length=18)
      */
     private $cardid;
 
     /**
-     * @var string
      *
-     * @ORM\Column(name="number", type="string", length=32, unique=true)
+     * @var string @ORM\Column(name="number", type="string", length=32, unique=true)
      */
     private $number;
 
     /**
-     * @var int
      *
-     * @ORM\Column(name="gender", type="smallint")
+     * @var int @ORM\Column(name="gender", type="smallint")
      */
     private $gender;
 
     /**
-     * @var int
      *
-     * @ORM\Column(name="mobile", type="bigint")
+     * @var int @ORM\Column(name="mobile", type="bigint")
      */
     private $mobile;
 
     /**
-     * @var int
      *
-     * @ORM\Column(name="city_id", type="integer")
+     * @var int @ORM\Column(name="city_id", type="integer")
      */
     private $cityId;
 
     /**
-     * @var int
      *
-     * @ORM\Column(name="suboffice_id", type="integer")
+     * @var int @ORM\Column(name="suboffice_id", type="integer")
      */
     private $subofficeId;
 
     /**
-     * @var string
      *
-     * @ORM\Column(name="job_title", type="string", length=64)
+     * @var string @ORM\Column(name="job_title", type="string", length=64)
      */
     private $jobTitle;
 
     /**
-     * @var int
      *
-     * @ORM\Column(name="admin_group_id", type="integer")
+     * @var int @ORM\Column(name="admin_group_id", type="integer")
      */
     private $adminGroupId;
-
 
     /**
      * Get id
@@ -113,14 +100,14 @@ class Admin extends BaseUser
     /**
      * Set username
      *
-     * @param string $username
+     * @param string $username            
      *
      * @return User
      */
     public function setUsername($username)
     {
         $this->username = $username;
-
+        
         return $this;
     }
 
@@ -137,14 +124,14 @@ class Admin extends BaseUser
     /**
      * Set password
      *
-     * @param string $password
+     * @param string $password            
      *
      * @return User
      */
     public function setPassword($password)
     {
         $this->password = $password;
-
+        
         return $this;
     }
 
@@ -161,14 +148,14 @@ class Admin extends BaseUser
     /**
      * Set truename
      *
-     * @param string $truename
+     * @param string $truename            
      *
      * @return User
      */
     public function setTruename($truename)
     {
         $this->truename = $truename;
-
+        
         return $this;
     }
 
@@ -185,14 +172,14 @@ class Admin extends BaseUser
     /**
      * Set cardid
      *
-     * @param string $cardid
+     * @param string $cardid            
      *
      * @return User
      */
     public function setCardid($cardid)
     {
         $this->cardid = $cardid;
-
+        
         return $this;
     }
 
@@ -209,14 +196,14 @@ class Admin extends BaseUser
     /**
      * Set number
      *
-     * @param string $number
+     * @param string $number            
      *
      * @return User
      */
     public function setNumber($number)
     {
         $this->number = $number;
-
+        
         return $this;
     }
 
@@ -233,14 +220,14 @@ class Admin extends BaseUser
     /**
      * Set gender
      *
-     * @param integer $gender
+     * @param integer $gender            
      *
      * @return User
      */
     public function setGender($gender)
     {
         $this->gender = $gender;
-
+        
         return $this;
     }
 
@@ -257,14 +244,14 @@ class Admin extends BaseUser
     /**
      * Set mobile
      *
-     * @param integer $mobile
+     * @param integer $mobile            
      *
      * @return User
      */
     public function setMobile($mobile)
     {
         $this->mobile = $mobile;
-
+        
         return $this;
     }
 
@@ -281,14 +268,14 @@ class Admin extends BaseUser
     /**
      * Set cityId
      *
-     * @param integer $cityId
+     * @param integer $cityId            
      *
      * @return User
      */
     public function setCityId($cityId)
     {
         $this->cityId = $cityId;
-
+        
         return $this;
     }
 
@@ -305,14 +292,14 @@ class Admin extends BaseUser
     /**
      * Set subofficeId
      *
-     * @param integer $subofficeId
+     * @param integer $subofficeId            
      *
      * @return User
      */
     public function setSubofficeId($subofficeId)
     {
         $this->subofficeId = $subofficeId;
-
+        
         return $this;
     }
 
@@ -329,14 +316,14 @@ class Admin extends BaseUser
     /**
      * Set jobTitle
      *
-     * @param string $jobTitle
+     * @param string $jobTitle            
      *
      * @return User
      */
     public function setJobTitle($jobTitle)
     {
         $this->jobTitle = $jobTitle;
-
+        
         return $this;
     }
 
@@ -353,14 +340,14 @@ class Admin extends BaseUser
     /**
      * Set adminGroupId
      *
-     * @param integer $adminGroupId
+     * @param integer $adminGroupId            
      *
      * @return User
      */
     public function setAdminGroupId($adminGroupId)
     {
         $this->adminGroupId = $adminGroupId;
-
+        
         return $this;
     }
 
@@ -373,5 +360,286 @@ class Admin extends BaseUser
     {
         return $this->adminGroupId;
     }
+
+    /**
+     * -----------------------------------------
+     * 继承interface必须实现的方法
+     * -----------------------------------------
+     */
+    
+    /**
+     * Gets the canonical username in search and sort queries.
+     *
+     * @return string
+     */
+    public function getUsernameCanonical()
+    {}
+
+    /**
+     * Sets the canonical username.
+     *
+     * @param string $usernameCanonical            
+     *
+     * @return self
+     */
+    public function setUsernameCanonical($usernameCanonical)
+    {}
+
+    /**
+     *
+     * @param string|null $salt            
+     */
+    public function setSalt($salt);
+    public function getSalt();
+    /**
+     * Gets email.
+     *
+     * @return string
+     */
+    public function getEmail()
+    {}
+
+    /**
+     * Sets the email.
+     *
+     * @param string $email            
+     *
+     * @return self
+     */
+    public function setEmail($email)
+    {}
+
+    /**
+     * Gets the canonical email in search and sort queries.
+     *
+     * @return string
+     */
+    public function getEmailCanonical()
+    {}
+
+    /**
+     * Sets the canonical email.
+     *
+     * @param string $emailCanonical            
+     *
+     * @return self
+     */
+    public function setEmailCanonical($emailCanonical)
+    {}
+
+    /**
+     * Gets the plain password.
+     *
+     * @return string
+     */
+    public function getPlainPassword()
+    {}
+
+    /**
+     * Sets the plain password.
+     *
+     * @param string $password            
+     *
+     * @return self
+     */
+    public function setPlainPassword($password)
+    {}
+
+    /**
+     * Tells if the the given user has the super admin role.
+     *
+     * @return bool
+     */
+    public function isSuperAdmin()
+    {}
+
+    /**
+     *
+     * @param bool $boolean            
+     *
+     * @return self
+     */
+    public function setEnabled($boolean)
+    {}
+
+    /**
+     * Sets the super admin status.
+     *
+     * @param bool $boolean            
+     *
+     * @return self
+     */
+    public function setSuperAdmin($boolean)
+    {}
+
+    /**
+     * Gets the confirmation token.
+     *
+     * @return string
+     */
+    public function getConfirmationToken()
+    {}
+
+    /**
+     * Sets the confirmation token.
+     *
+     * @param string $confirmationToken            
+     *
+     * @return self
+     */
+    public function setConfirmationToken($confirmationToken)
+    {}
+
+    /**
+     * Sets the timestamp that the user requested a password reset.
+     *
+     * @param null|\DateTime $date            
+     *
+     * @return self
+     */
+    public function setPasswordRequestedAt(\DateTime $date = null)
+    {}
+
+    /**
+     * Checks whether the password reset request has expired.
+     *
+     * @param int $ttl
+     *            Requests older than this many seconds will be considered expired
+     *            
+     * @return int
+     */
+    public function isPasswordRequestNonExpired($ttl)
+    {}
+
+    /**
+     * Sets the last login time.
+     *
+     * @param \DateTime $time            
+     *
+     * @return self
+     */
+    public function setLastLogin(\DateTime $time = null)
+    {}
+
+    /**
+     * Never use this to check if this user has access to anything!
+     *
+     * Use the AuthorizationChecker, or an implementation of AccessDecisionManager
+     * instead, e.g.
+     *
+     * $authorizationChecker->isGranted('ROLE_USER');
+     *
+     * @param string $role            
+     *
+     * @return bool
+     */
+    public function hasRole($role)
+    {}
+
+    /**
+     * Sets the roles of the user.
+     *
+     * This overwrites any previous roles.
+     *
+     * @param array $roles            
+     *
+     * @return self
+     */
+    public function setRoles(array $roles)
+    {}
+    public function getRoles()
+    {}
+
+    /**
+     * Adds a role to the user.
+     *
+     * @param string $role            
+     *
+     * @return self
+     */
+    public function addRole($role)
+    {}
+
+    /**
+     * Removes a role to the user.
+     *
+     * @param string $role            
+     *
+     * @return self
+     */
+    public function removeRole($role)
+    {}
+
+    /**
+     * Checks whether the user's account has expired.
+     *
+     * Internally, if this method returns false, the authentication system
+     * will throw an AccountExpiredException and prevent login.
+     *
+     * @return bool true if the user's account is non expired, false otherwise
+     *        
+     * @see AccountExpiredException
+     */
+    public function isAccountNonExpired()
+    {}
+
+    /**
+     * Checks whether the user is locked.
+     *
+     * Internally, if this method returns false, the authentication system
+     * will throw a LockedException and prevent login.
+     *
+     * @return bool true if the user is not locked, false otherwise
+     *        
+     * @see LockedException
+     */
+    public function isAccountNonLocked()
+    {}
+
+    /**
+     * Checks whether the user's credentials (password) has expired.
+     *
+     * Internally, if this method returns false, the authentication system
+     * will throw a CredentialsExpiredException and prevent login.
+     *
+     * @return bool true if the user's credentials are non expired, false otherwise
+     *        
+     * @see CredentialsExpiredException
+     */
+    public function isCredentialsNonExpired()
+    {}
+
+    /**
+     * Checks whether the user is enabled.
+     *
+     * Internally, if this method returns false, the authentication system
+     * will throw a DisabledException and prevent login.
+     *
+     * @return bool true if the user is enabled, false otherwise
+     *        
+     * @see DisabledException
+     */
+    public function isEnabled()
+    {}
+
+    public function serialize()
+    {}
+
+    /**
+     *
+     * @param
+     *            serialized
+     */
+    public function unserialize($serialized)
+    {}
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
+    {}
 }
 
