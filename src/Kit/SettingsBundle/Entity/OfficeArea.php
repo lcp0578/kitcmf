5e5774f901b9595803552d07b45bc7196e6792be
李春鹏 <lcp0578@gmail.com>
2016-12-20 15:09:31 +0800

    [MOD] add office area

---
+++ src/Kit/SettingsBundle/Entity/OfficeArea.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace Kit\SettingsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * OfficeArea
+ *
+ * @ORM\Table(name="office_area")
+ * @ORM\Entity(repositoryClass="Kit\SettingsBundle\Repository\OfficeAreaRepository")
+ */
+class OfficeArea
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=64)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="pid", type="integer")
+     */
+    private $pid;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="status", type="integer")
+     */
+    private $status;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="create_at", type="datetime")
+     */
+    private $createAt;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return OfficeArea
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set pid
+     *
+     * @param integer $pid
+     *
+     * @return OfficeArea
+     */
+    public function setPid($pid)
+    {
+        $this->pid = $pid;
+
+        return $this;
+    }
+
+    /**
+     * Get pid
+     *
+     * @return integer
+     */
+    public function getPid()
+    {
+        return $this->pid;
+    }
+
+    /**
+     * Set status
+     *
+     * @param integer $status
+     *
+     * @return OfficeArea
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return int
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Set createAt
+     *
+     * @param \DateTime $createAt
+     *
+     * @return OfficeArea
+     */
+    public function setCreateAt($createAt)
+    {
+        $this->createAt = $createAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createAt
+     *
+     * @return \DateTime
+     */
+    public function getCreateAt()
+    {
+        return $this->createAt;
+    }
+}
+
