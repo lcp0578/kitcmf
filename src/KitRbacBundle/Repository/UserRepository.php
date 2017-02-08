5355118818d7e4722f9b4e9bb77c94e083c60a13
李春鹏 <lcp0578@gmail.com>
2017-02-08 17:28:43 +0800

    [MOD] add create query builder

--- src/KitRbacBundle/Repository/UserRepository.php
+++ src/KitRbacBundle/Repository/UserRepository.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace KitRbacBundle\Repository;
 
 /**
@@ -10,14 +9,36 @@ namespace KitRbacBundle\Repository;
  */
 class UserRepository extends \Doctrine\ORM\EntityRepository
 {
-    
+    /**
+     * Query Builder is an api to construct queries, 
+     * so it's easier if you need to build a query 
+     * dynamically like iterating over a set of parameters 
+     * or filters.
+     * 
+     */
     public function getList()
     {
-        $query = $this->getEntityManager()->createQuery(
-                'SELECT u.id, u.username,u.createAt,u.updateAt, r.rolename FROM KitRbacBundle:User u 
-                JOIN u.role r
-                WHERE u.status = :status'
-            )->setParameter('status', 1);
-        return $query->getResult();
+        return $this->createQueryBuilder('u')
+            ->select('u.id', 'u.username', 'u.createAt', 'u.updateAt', 'r.rolename')
+            ->join('u.role', 'r')
+            //->where('u.status = ?0')
+            //->setParameter(0, 1) // key, The parameter position or name
+            ->where('u.status = :status')
+            ->setParameter('status', 1)
+            ->getQuery()
+            ->getResult();
+        
+       
+    }
+    /**
+     * DQL is easier to read as it is very similar to SQL. 
+     */
+    public function getList2()
+    {
+        return $this->getEntityManager()->createQuery(
+            'SELECT u.id, u.username,u.createAt,u.updateAt, r.rolename FROM KitRbacBundle:User u
+            JOIN u.role r
+            WHERE u.status = :status'
+        )->setParameter('status', 1)->getResult();
     }
 }
