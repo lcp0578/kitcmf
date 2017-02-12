04f746e50b6d8185abd904cd7e580dbad3ae8209
李春鹏 <lcp0578@gmail.com>
2017-02-12 09:41:50 +0800

    [MOD] add web news

---
+++ src/KitWebBundle/Controller/NewsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace KitWebBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class NewsController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('KitWebBundle:News:index.html.twig');
+    }
+    
+    public function showAction()
+    {
+        return $this->render('KitWebBundle:News:show.html.twig');
+    }
+}
