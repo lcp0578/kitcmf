db1c488c172492af5af9633e0e9943ac2760e736
李春鹏 <lcp0578@gmail.com>
2017-02-12 19:03:23 +0800

    [MOD] modify waterfall

--- src/KitWebBundle/Controller/NewsController.php
+++ src/KitWebBundle/Controller/NewsController.php
@@ -1,18 +1,67 @@
 <?php
-
 namespace KitWebBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class NewsController extends Controller
 {
+
     public function indexAction()
     {
         return $this->render('KitWebBundle:News:index.html.twig');
     }
-    
+
     public function showAction($uuid)
     {
         return $this->render('KitWebBundle:News:show.html.twig');
+    }
+
+    public function dataAction(Request $request)
+    {
+        $page = $request->get('page', 2);
+        $json = [
+            'total' => 20,
+            'result' => [
+                [
+                    "image" => "http://wlog.cn/demo/waterfall/images/001.jpg",
+                    "width" => 192,
+                    "height" => 288
+                ],
+                [
+                    "image" => "http://wlog.cn/demo/waterfall/images/001.jpg",
+                    "width" => 192,
+                    "height" => 288
+                ],
+                [
+                    "image" => "http://wlog.cn/demo/waterfall/images/001.jpg",
+                    "width" => 192,
+                    "height" => 288
+                ],
+                [
+                    "image" => "http://wlog.cn/demo/waterfall/images/001.jpg",
+                    "width" => 192,
+                    "height" => 288
+                ],
+                [
+                    "image" => "http://wlog.cn/demo/waterfall/images/001.jpg",
+                    "width" => 192,
+                    "height" => 288
+                ],
+                [
+                    "image" => "http://wlog.cn/demo/waterfall/images/001.jpg",
+                    "width" => 192,
+                    "height" => 288
+                ],
+                [
+                    "image" => "http://wlog.cn/demo/waterfall/images/001.jpg",
+                    "width" => 192,
+                    "height" => 288
+                ]
+            ]
+        ];
+        
+        return new JsonResponse($json);
     }
 }
