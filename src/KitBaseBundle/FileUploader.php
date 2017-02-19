4944a00c8ce848d65e6bf382a676b6c68a83cbcd
李春鹏 <lcp0578@gmail.com>
2017-02-19 23:12:47 +0800

    [MOD] add file uploader

---
+++ src/KitBaseBundle/FileUploader.php
@@ -0,0 +1,23 @@
+<?php
+namespace KitBaseBundle;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class FileUploader
+{
+    private $targetDir;
+
+    public function __construct($targetDir)
+    {
+        $this->targetDir = $targetDir;
+    }
+
+    public function upload(UploadedFile $file)
+    {
+        $fileName = md5(uniqid()).'.'.$file->guessExtension();
+
+        $file->move($this->targetDir, $fileName);
+
+        return $fileName;
+    }
+}
