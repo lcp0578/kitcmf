cd6b67b5cc0ae561a8a636b53d439886ca3b5890
李春鹏 <lcp0578@gmail.com>
2017-02-20 19:25:26 +0800

    [MOD] add guzzle http

--- src/KitDnspodBundle/Controller/DefaultController.php
+++ src/KitDnspodBundle/Controller/DefaultController.php
@@ -3,11 +3,18 @@
 namespace KitDnspodBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('KitDnspodBundle:Default:index.html.twig');
+    }
+    
+    public function testAction()
+    {
+        $guzzleHttp = $this->get('kit.guzzle_http');
+        return new JsonResponse();
     }
 }
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -8,6 +8,9 @@ class ComposerStaticInit11ed4cd401fa9de7d75d42ec4870d15d
 {
     public static $files = array (
         '92c8763cd6170fce6fcfe7e26b4e8c10' => __DIR__ . '/..' . '/symfony/phpunit-bridge/bootstrap.php',
+        'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
+        'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
+        '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
         '2c102faa651ef8ea5874edb585946bce' => __DIR__ . '/..' . '/swiftmailer/swiftmailer/lib/swift_required.php',
         '5255c38a0faeba867671b61dfda6d864' => __DIR__ . '/..' . '/paragonie/random_compat/lib/random.php',
         '023d27dca8066ef29e6739335ea73bad' => __DIR__ . '/..' . '/symfony/polyfill-php70/bootstrap.php',
@@ -47,6 +50,7 @@ class ComposerStaticInit11ed4cd401fa9de7d75d42ec4870d15d
         'P' => 
         array (
             'Psr\\Log\\' => 8,
+            'Psr\\Http\\Message\\' => 17,
             'Psr\\Cache\\' => 10,
         ),
         'N' => 
@@ -70,6 +74,9 @@ class ComposerStaticInit11ed4cd401fa9de7d75d42ec4870d15d
         ),
         'G' => 
         array (
+            'GuzzleHttp\\Psr7\\' => 16,
+            'GuzzleHttp\\Promise\\' => 19,
+            'GuzzleHttp\\' => 11,
             'Gregwar\\Captcha\\' => 16,
             'Gregwar\\CaptchaBundle\\' => 22,
         ),
@@ -178,6 +185,10 @@ class ComposerStaticInit11ed4cd401fa9de7d75d42ec4870d15d
         array (
             0 => __DIR__ . '/..' . '/psr/log/Psr/Log',
         ),
+        'Psr\\Http\\Message\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/http-message/src',
+        ),
         'Psr\\Cache\\' => 
         array (
             0 => __DIR__ . '/..' . '/psr/cache/src',
@@ -209,6 +220,18 @@ class ComposerStaticInit11ed4cd401fa9de7d75d42ec4870d15d
         'Incenteev\\ParameterHandler\\' => 
         array (
             0 => __DIR__ . '/..' . '/incenteev/composer-parameter-handler',
+        ),
+        'GuzzleHttp\\Psr7\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/guzzlehttp/psr7/src',
+        ),
+        'GuzzleHttp\\Promise\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/guzzlehttp/promises/src',
+        ),
+        'GuzzleHttp\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/guzzlehttp/guzzle/src',
         ),
         'Gregwar\\Captcha\\' => 
         array (
