cf53eee6ba240917b40edc104d04e4ac44929f8d
李春鹏 <lcp0578@gmail.com>
2017-02-21 19:13:25 +0800

    [MOD] add record controller

---
+++ src/KitDnspodBundle/Controller/RecordController.php
@@ -0,0 +1,13 @@
+<?php
+namespace KitDnspodBundle\Controller;
+
+
+class RecordController extends BaseController
+{
+
+    public function indexAction()
+    {
+        return $this->render('KitDnspodBundle:Default:index.html.twig');
+    }
+
+}
