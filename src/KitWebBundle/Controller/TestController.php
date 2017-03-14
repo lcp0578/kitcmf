9b969d7fec7ca77a32ec1916f88aca8189ede28a
李春鹏 <lcp0578@gmail.com>
2017-03-14 19:18:21 +0800

    [MOD] add test controller

---
+++ src/KitWebBundle/Controller/TestController.php
@@ -0,0 +1,42 @@
+<?php
+namespace KitWebBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use KitWebBundle\Form\WebUserType;
+use KitWebBundle\Entity\WebUser;
+use Symfony\Component\HttpFoundation\Response;
+
+class TestController extends Controller
+{
+
+    public function indexAction(Request $request)
+    {
+        $type = $request->query->get('versionType', 'default version type');
+        $version = $request->query->get('curVersion', 'current version');
+        $desVersion = '3.0.0.0';
+        $versions = [
+            '2.9.8.0_wo0' => '3.0.0.1',
+            '2.9.0.0_bcs6' => '3.0.0.1'
+        ];
+        if(array_key_exists($version, $versions)){
+            $desVersion = $versions[$version];
+        }
+        return new Response('gamebox|' . $desVersion);
+    }
+    
+    public function downloadAction(Request $request)
+    {
+        $type = $request->query->get('versionType', 'default version type');
+        $version = $request->query->get('curVersion', 'current version');
+        $desVersion = '3.0.0.0';
+        $versions = [
+            '2.9.8.0_wo0' => '3.0.0.1',
+            '2.9.0.0_bcs6' => '3.0.0.1'
+        ];
+        if(array_key_exists($version, $versions)){
+            $desVersion = $versions[$version];
+        }
+        return new Response('gamebox|' . $desVersion);
+    }
+}
