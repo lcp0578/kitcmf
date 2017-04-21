ff1c09145a8c2ce178b4af3c4855df3013a1cce4
李春鹏 <lcp0578@gmail.com>
2017-04-21 18:12:52 +0800

    [MOD] modify domain list

--- src/KitDnspodBundle/Controller/DomainController.php
+++ src/KitDnspodBundle/Controller/DomainController.php
@@ -3,6 +3,7 @@ namespace KitDnspodBundle\Controller;
 
 
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 class DomainController extends BaseController
 {
 
@@ -11,17 +12,36 @@ class DomainController extends BaseController
         return $this->render('KitDnspodBundle:Domain:index.html.twig');
     }
     
-    public function listAction()
+    public function listAction($page, Request $request)
     {
-        $result = $this->request('Domain.List');
-        // 17kxgame 37951496
-        return new JsonResponse($result);
+        $groupId = $request->query->get('group_id', null);
+        if($page < 1) $page = 1;
+        $pagesize = 10;
+        $param = [];
+        if(!is_null($groupId)){
+            $param['group_id'] = $groupId; 
+        }
+        $result = $this->request('Domain.List', $param);
+        if(200 == $result['code'] && isset($result['data']['domains'])){
+            $list = $result['data']['domains'];
+        }else{
+            return new JsonResponse('网络异常，请刷新');
+        }
+        $paginator = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $list,
+            $page,
+            $pagesize
+        );
+        return $this->render('KitDnspodBundle:Domain:index.html.twig', [
+            'pagination' => $pagination
+        ]);
     }
     
     public function groupAction($page)
     {
         if($page < 1) $page = 1;
-        $pagesize = 3;
+        $pagesize = 10;
         $result = $this->request('Domaingroup.List');
         if(200 == $result['code'] && isset($result['data']['groups'])){
             $list = $result['data']['groups'];
@@ -37,7 +57,5 @@ class DomainController extends BaseController
         return $this->render('KitDnspodBundle:Domain:group.html.twig', [
             'pagination' => $pagination
         ]);
-        // 17kxgame 37951496
-        return new JsonResponse($result);
     }
 }
